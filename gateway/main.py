67eb930a72b07f8753e1f43c16ef81df27d977e7
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-07-04 04:04:27 +0300

    created api gateway, filled methods of job api

---
+++ gateway/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+JOB_API_URL = "http://localhost:5001"
+GPT_URL = "http://localhost:5002"
+
+def proxy_request(target_url):
+    method = request.method
+    headers = {key: value for key, value in request.headers if key != 'Host'}
+    data = request.get_data()
+    params = request.args
+
+    resp = requests.request(method, target_url, headers=headers, params=params, data=data)
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+
+    return Response(resp.content, resp.status_code, headers)
+
+@app.route("/ping_gateway", methods = ["GET", "POST", "PATCH", "DELETE"])
+def ping_gateway():
+    message =  "pinged python api gateway"
+    return Response(message, 200, mimetype='text/plain')
+
+@app.route("/job/<path:path>", methods = ["GET", "POST", "PATCH", "DELETE"])
+def llm_proxy(path):
+    actual_path = f"{JOB_API_URL}/{path}"
+    return proxy_request(actual_path)
+
+@app.route("/gpt_api/<path:path>", methods = ["GET", "POST", "PATCH", "DELETE"])
+def spring_proxy(path):
+    actual_path = f"{GPT_URL}/{path}"
+    return proxy_request(actual_path)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug = True) # Note: don't forget to turn off debug later on
--- job-api/main.py
+++ job-api/main.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import db, JobPosition
 
 
 app = Flask(__name__)
-
 
 def create_db():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -15,23 +14,22 @@ def create_db():
     with app.app_context():
         db.create_all()
 
-
 @app.route("/ping")
 def hello():
     return "pong"
 
-
 # /getJobs
 @app.route("/getJobs", methods=["GET", "POST"])
 def getJobs():
-    pass
 
+    return jsonify("pong")
+    pass
 
 @app.route("/addJobListing", methods=["POST"])
 def addJobListing():
     pass
 
-
 if __name__ == "__main__":
     create_db()
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
+
